ba46074e58e547911c01535aef414d6d16028b6d
yannisVoynnet <yvoynnet@gmail.com>
2024-10-14 15:37:10 +0200

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Indique que le projet est hébergé dans un sous-répertoire
   plugins: [react()],
-})
+});
