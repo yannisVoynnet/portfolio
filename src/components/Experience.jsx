84d8856f4c316390d05e41286cebbe7fe526d00d
yannisVoynnet <yvoynnet@gmail.com>
2024-11-07 16:05:14 +0100

    download pdf ok

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'resume link', //paste the link to your resume here
+                  'public/CV_Yannis_Voynnet.pdf', //paste the link to your resume here
                   '_blank'
                 )
               }
